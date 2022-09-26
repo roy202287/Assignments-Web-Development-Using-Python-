bd4c079d40bc2942ef53ca13169b1b3492492e1f
roy202287 <112996655+roy202287@users.noreply.github.com>
2022-09-26 14:53:53 +0530

    Add files via upload

---
+++ Assignment4.py
@@ -0,0 +1,65 @@
+#1
+'''
+a=input("Enter your name: ")
+print(a,type(a),sep='\n')
+'''
+#2
+'''
+b=int(input("Enter a number: "))
+print(b)
+'''
+#3
+'''
+a=int(input("Enter 1st number: "))
+b=int(input("Enter 2nd number: "))
+print("Sum of %d and %d is %d" %(a,b,a+b)) #Formatted String
+'''
+#4
+'''
+r=int(input("Enter the radius of the circle: "))
+a=3.14*r*r
+print("The area of circle of radius %d is %f" %(r,a))
+'''
+#5
+'''
+a=int(input("Enter a number: "))
+print("The square of the number is %f" %(a**2))
+'''
+#6
+'''
+b=int(input("Enter the base : "))
+h=int(input("Enter the height: "))
+a=((b*h)/2)
+print("The are of triangle is %d" %a)
+'''
+#7
+'''
+n1=int(input("Enter the 1st number: "))
+n2=int(input("Enter the 2nd number: "))
+n3=int(input("Enter the 3rd number: "))
+n4=(n1+n2+n3)/3
+print("Average of %d , %d and %d is %d" %(n1,n2,n3,n4))
+'''
+#8
+'''
+P=int(input("Enter the Principal : "))
+r=int(input("Enter the interest rate : "))
+t=int(input("Enter the time : "))
+A=((P*r*t)/100)
+print("The Simple Interest amount is ",A)
+'''
+#9
+'''
+l=int(input("Enter the length: "))
+b=int(input("Enter the breadth: "))
+h=int(input("Enter the height: "))
+a=l*b*h
+print("The area of the cuboid of the dimensions l=%d b=%d and h=%d is %d sq"%(l,b,h,a))
+'''
+#10
+'''
+l=int(input("Enter the length: "))
+b=int(input("Enter the breadth: "))
+a=l*b
+print("The area of the rectangle is ",a)
+'''
