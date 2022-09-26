1ed0cdb7ddd8198484fba4ad07eb5e04f34cbdba
roy202287 <112996655+roy202287@users.noreply.github.com>
2022-09-26 18:52:31 +0530

    Add files via upload

---
+++ Assignment8.py
@@ -0,0 +1,75 @@
+#Print MysirG n times.
+'''
+i=1
+while i<=5:
+    print("MySirG")
+    i=i+1
+'''
+#Print first 10 natural numbers
+'''
+i=1
+while i<=10:
+    print(i,end=' ')
+    i+=1
+'''
+#Print first 10 natural numbers in reverse order.
+'''
+i=10
+while i>0:
+    print(i,end=' ')
+    i-=1
+'''
+#Print first 10 odd natural numbers.
+'''
+i=1
+while i<20: # 2n-1
+    if i%2 != 0:
+        print(i,end=' ')
+    i=i+1
+'''
+#Print first 10 odd numbers in reverse order
+'''
+i=19
+while i>0:
+    if i%2 != 0:
+        print(i,end=' ')
+    i=i-1
+'''
+#Print first 10 even natural numbers
+'''
+i=1
+while i<=20: # 2n
+    if i%2 == 0:
+        print(i,end=' ')
+    i=i+1
+'''
+#Print first 10 even natural numbers in reverse order.
+'''
+i=20
+while i>0: # 2n
+    if i%2 == 0:
+        print(i,end=' ')
+    i=i-1
+'''
+#Print the square of first 10 natural numbers.
+'''
+i=1
+while i<=10:
+    print((i**2),end=' ')
+    i+=1
+'''
+#Print the cubes of first 10 natural numbers.
+'''
+i=1
+while i<=10:
+    print((i**3),end=' ')
+    i+=1
+'''
+#Print the first 10 multiples of 5 
+'''
+i=1
+while i<=50:
+    if i%5 == 0:
+        print(i,end=' ')
+    i+=1
+'''
