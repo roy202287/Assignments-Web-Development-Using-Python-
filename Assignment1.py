eddcc4c10f45039323c403f86945013b5e22ce9c
roy202287 <112996655+roy202287@users.noreply.github.com>
2022-09-26 15:08:11 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,44 @@
+#1
+'''
+import sys
+print(sys.version)
+'''
+#2
+'''
+print("Hello Python")
+'''
+#3
+'''
+print("Hello Python")
+'''
+#4
+'''
+print("Hello Python")
+'''
+#5
+'''
+print("Hello \n Python")
+'''
+#6
+'''
+print(" \" MySirG \" ")
+'''
+#7
+'''
+print(" \" Teacher's Day \" ")
+'''
+#8
+'''
+a='sourav'
+print(a)
+'''
+#9
+'''
+a,b,c,d='sourav',22,'Btech',0
+print(a,b,c,d)
+'''
+#10
+'''
+a,b,c=1,2,3
+print(a,b,c,sep='\n')
+'''
